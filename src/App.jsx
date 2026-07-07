d4a957c0ab4ec9d7ab1b97228b32aca4bf24b94b
BLACK AURA <blackaurawomen11@gmail.com>
2026-07-07 03:21:27 +0300

    Switch to HashRouter for GitHub Pages compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
 import Navbar from './components/Navbar'
 import AnnouncementBar from './components/AnnouncementBar'
@@ -37,7 +37,7 @@ function HomePage() {
 
 export default function App() {
   return (
-    <BrowserRouter basename="/black-aura-store">
+    <HashRouter>
       <AuthProvider>
         <div className="min-h-screen bg-deep-charcoal text-cream-white overflow-x-hidden">
           <SupabaseSync />
@@ -64,6 +64,6 @@ export default function App() {
           <Footer />
         </div>
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
